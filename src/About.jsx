60cdb3bbeebc91592cc79dcab2ff8acb4a1f3c35
Maija Grimes <maija.grimes@colorado.edu>
2026-09-02 22:35:19 -0500

    css updates

--- src/About.jsx
+++ src/About.jsx
@@ -16,18 +16,26 @@ export default function About() {
     const [order, setOrder] = useState(photos.map((_, i) => i))
 
     const aboutLong = (
-        <div className='about-right'>
+        <>
             <p>Hello world!</p>
             <p>My name is Maija -- pronounced "MY-ah," like Maya or Maia -- and it's spelled differently because it's Finnish! 🇫🇮</p>
             <p>I'm a recent computer science graduate with a love for all things creative, both online and offline. I spent most of my childhood convinced I'd grow up to be an artist, and I kinda feel like one when I spend a lot of time in a CSS file.</p>
             <p>I first touched a line of code as a sophomore in high school taking a Web Page Development class. I learned a tiny bit of HTML and CSS and had way too much fun making genuinely awful websites (you can still visit my first one <a href="https://maijas-first-website.neocities.org/" target='blank_'>here</a>).</p>
             <p>I liked it so much that I signed up for a college-level CS course and the teacher said I was one of the best students they'd had in the class (I swear!). Next thing I know, I've finished a bachelor's degree in computer science.</p>
             <p>Now I'm here and I still love coding and web development as much as when I first started, but with a whole lot more tools in my tool kit. Keep scrolling to check out some of my creations!</p>
-        </div>
+        </>
     )
 
     const aboutShort = (
         <>
+            <ul>
+                <li>My name is Maija</li>
+                <li>I'm a recent CS graduate</li>
+                <li>I love to be creative</li>
+                <li>I first learned HTML & CSS in 2019</li>
+                <li>After 7 years I still love coding and web dev</li>
+                <li>Keep scrolling to check out my projects page</li>
+            </ul>
         </>
     )
 
@@ -78,8 +86,14 @@ export default function About() {
                         </div>
                         <h1>About Me</h1>
                     </div>
-                    <div>
-                        {aboutContent}
+                    <div className='about-right-outer'>
+                        <button className='about-toggle' onClick={() => setShowAll(p => !p)}>
+                            {showAll ? 'Read more' : 'Summarize'}
+                        </button>
+                        <div className='about-right'>
+                            {aboutContent}
+                        </div>
+                        
                     </div>
                 </div>
             </section>
